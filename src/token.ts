9de3506735cab0a2ab6233a71f5c9f5caf881586
Langning Chen <chenlangning2009@outlook.com>
2025-01-12 20:36:41 +0800

    Extend Tokens

--- src/token.ts
+++ src/token.ts
@@ -1,20 +1,23 @@
+import { PrismaClient } from '@prisma/client';
 import { config } from './config';
 import { prisma } from './db';
 import { setIntervalImmediately } from './utils';
 
 export class Tokens {
+    constructor(private readonly prismaToken: PrismaClient['token']) { }
+
     async fetchBlankTokenInterval() {
         setIntervalImmediately(async () => {
-            const tokens = await prisma.token.findMany({ where: { token: null }, });
+            const tokens = await this.prismaToken.findMany({ where: { token: null }, });
             if (tokens.length > 0) { this.fetchToken(tokens.map(token => token.uid)); }
         }, config.token.interval);
     }
 
-    async fetchToken(uidList?: number[]) {
-        for (const uid of uidList || (await prisma.token.findMany()).map(token => token.uid)) {
-            const token = await prisma.token.findUnique({ where: { uid, }, });
+    async fetchToken(uidList: number[]) {
+        for (const uid of uidList) {
+            const token = await this.prismaToken.findUnique({ where: { uid, }, });
             if (!token) { continue; }
-            await prisma.token.update({ where: { uid, }, data: { token: null, message: 'Getting token', }, });
+            await this.prismaToken.update({ where: { uid, }, data: { token: null, message: 'Getting token', }, });
             try {
                 const res = await fetch(`${config.socket.http}/api/auth/gettoken`, {
                     method: 'POST',
@@ -23,16 +26,16 @@ export class Tokens {
                 });
                 if (res.status !== 200) { throw 'Request token failed'; }
                 const data = await res.json();
-                if (data.data.errorType) { await prisma.token.update({ where: { uid, }, data: { message: `${data.data.errorType} ${data.data.message}`, }, }); }
-                else { await prisma.token.update({ where: { uid, }, data: { token: data.data.token, message: null, }, }); }
+                if (data.data.errorType) { await this.prismaToken.update({ where: { uid, }, data: { message: `${data.data.errorType} ${data.data.message}`, }, }); }
+                else { await this.prismaToken.update({ where: { uid, }, data: { token: data.data.token, message: null, }, }); }
             } catch (err) {
-                await prisma.token.update({ where: { uid, }, data: { message: `Request token failed: ${err}`, }, });
+                await this.prismaToken.update({ where: { uid, }, data: { message: `Request token failed: ${err}`, }, });
             }
         }
     }
 
-    async getToken(uid: number) { return await prisma.token.findUnique({ where: { uid, }, }); }
-    async getTokens() { return await prisma.token.findMany(); }
+    async getToken(uid: number) { return await this.prismaToken.findUnique({ where: { uid, }, }); }
+    async getTokens() { return await this.prismaToken.findMany(); }
 
     async isCooledDown(uid: number) {
         const token = await this.getToken(uid);
@@ -42,13 +45,13 @@ export class Tokens {
     }
 
     async getAvailableToken() {
-        const token = await prisma.token.findFirst({ where: { token: { not: null }, lastUsed: { lt: new Date(new Date().getTime() - config.token.cd) }, }, });
+        const token = await this.prismaToken.findFirst({ where: { token: { not: null }, lastUsed: { lt: new Date(new Date().getTime() - config.token.cd) }, }, });
         if (!token) { return null; }
         return { uid: token.uid, token: token.token! };
     }
 
-    async updateUseTime(uid: number, lastUsed: Date) { await prisma.token.update({ where: { uid, }, data: { lastUsed, }, }); }
+    async updateUseTime(uid: number, lastUsed: Date) { await this.prismaToken.update({ where: { uid, }, data: { lastUsed, }, }); }
 }
 
-export const tokens = new Tokens();
+export const tokens = new Tokens(prisma.token);
 tokens.fetchBlankTokenInterval();
