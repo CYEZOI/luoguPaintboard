59deaf66144facb7799b2443bba99e990fc8c2f7
Langning Chen <chenlangning2009@outlook.com>
2025-01-17 17:15:42 +0800

    Systeminformation isksIO does not exists on Windows

--- public/monitor.js
+++ public/monitor.js
@@ -16,8 +16,12 @@ export class MONITOR {
             this.systemInfo.innerHTML = `OS: ${data.osInfo.platform}   ${data.osInfo.distro} ${data.osInfo.release} ${data.osInfo.arch}   ${data.osInfo.hostname}
 CPU: ${data.currentLoad.currentLoad.toFixed(2)}%   ${data.currentLoad.cpus.map((cpu) => cpu.load.toFixed(2))}
 Memory: ${(data.mem.used / data.mem.total * 100).toFixed(2)}%   ${(data.mem.used / 1024 / 1024).toFixed(2)}MB / ${(data.mem.total / 1024 / 1024).toFixed(2)}MB
-DiskIO: Read ${(data.disksIO.rIO / 1024 / 1024).toFixed(2)}MB/s   Write ${(data.disksIO.wIO / 1024 / 1024).toFixed(2)}MB/s
-Network: In ${((currentIn - lastIn) / 1024).toFixed(2)}KB/s   Out ${((currentOut - lastOut) / 1024).toFixed(2)}KB/s`;
+`;
+            if (data.disksIO) {
+                this.systemInfo.innerHTML += `DiskIO: Read ${(data.disksIO.rIO / 1024 / 1024).toFixed(2)}MB/s   Write ${(data.disksIO.wIO / 1024 / 1024).toFixed(2)}MB/s
+`;
+            }
+            this.systemInfo.innerHTML += `Network: In ${((currentIn - lastIn) / 1024).toFixed(2)}KB/s   Out ${((currentOut - lastOut) / 1024).toFixed(2)}KB/s`;
             lastIn = currentIn, lastOut = currentOut;
         };
         monitorSocket.addEventListener('close', () => {
